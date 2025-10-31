da94bfe35af6dd49aa61993ff50386ce07770796
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-10-31 19:11:02 +0800

    chore: update

--- 0004-DAP/src/main.py
+++ 0004-DAP/src/main.py
@@ -2,20 +2,11 @@ HW = "Hello World"
 
 
 def main():
-    try:
-        temp = ""
-    except Exception as e:
-        print(e)
     start = input("Please enter start number: ")
     end = input("Please enter end number: ")
     step = input("Please enter step number: ")
     for attempt in range(int(start), int(end), int(step)):
         print(f"Attempt #{attempt}")
-    pass
-
-    pass
-
-    return temp
 
 
 if __name__ == "__main__":
