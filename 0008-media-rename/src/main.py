dab946bff39c8a2e4631eb220f08380d045ee67f
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2026-02-02 12:28:45 +0800

    chore: add script to update video filename using datetime

--- 0008-media-rename/src/main.py
+++ 0008-media-rename/src/main.py
@@ -1,8 +1,8 @@
 # ruff: noqa: F401
-
 from datetime import datetime
 from pathlib import Path
 
+import ffmpeg
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -20,7 +20,6 @@ for file_path in files_list:
     file_extension = file_path.suffix.lower()
     if file_extension in img_suffix:
         media = Path(file_path)
-        print(media)
         with Image.open(media) as image:
             exif = image._getexif()
             for tag, value in exif.items():
@@ -39,6 +38,30 @@ for file_path in files_list:
                         value, "%Y:%m:%d %H:%M:%S"
                     ).strftime("%Y%m%d_%H%M%S")
                     new_name = media_dir / f"{name_datetime}{file_extension}"
+        if not new_name:
+            continue
+        try:
+            media.rename(new_name)
+        except FileExistsError:
+            count = 1
+            while new_name.exists():
+                count += 1
+                new_name = media_dir / f"{name_datetime}_{count}{file_extension}"
+            media.rename(new_name)
+        finally:
+            new_name = ""
+
+    elif file_extension in video_suffix:
+        media = Path(file_path)
+        probe = ffmpeg.probe(file_path)
+        for stream in probe["streams"]:
+            if stream["codec_type"] == "video":
+                creation_time_iso = datetime.fromisoformat(
+                    stream["tags"]["creation_time"]
+                )
+                name_datetime = creation_time_iso.strftime("%Y%m%d_%H%M%S")
+                new_name = media_dir / f"{name_datetime}{file_extension}"
+                break
         if not new_name:
             continue
         try:
